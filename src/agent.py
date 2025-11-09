9cfd077ac5b0303695bb85fe767b0ee547261055
Tim <tim_julian_oswald@yahoo.com>
2025-11-09 19:59:03 +0100

    Add first version of the agent and the environment

---
+++ src/agent.py
@@ -0,0 +1,93 @@
+"""
+Here you can find the code of the following tutorial:
+
+https://docs.pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
+
+"""
+
+
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+from environment import Game2048Env
+
+Transition = namedtuple(
+    'Transition', ('state', 'action', 'next_state', 'reward'))
+
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+def DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super().__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+
+BATCH_SIZE = 128
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.01
+EPS_DECAY = 2500
+TAU = 0.005
+LR = 3e-4
+
+env = Game2048Env()
+
+n_actions = env.action_space.n
+state, info = env.reset()
+n_observations = len(state)
+
+policy_net = DQN(n_observations, n_actions)
+target_net = DQN(n_observations, n_actions)
+target_net.load_state_dict(policy_net.state_dict())
+
+optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+memory = ReplayMemory(10000)
+
+steps_done = 0
+
+
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+    if sample > eps_threshold:
+        with torch.no_grad():
+            return policy_net(state).max(1).indices.view(1, 1)
+    else:
+        return torch.tensor([[env.action_space.sample()]], dtype=torch.long)
+
+
+episode_durations = []
